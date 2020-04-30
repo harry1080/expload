40bbe1a4500e9f1bb8f2012923156376d2c95633
hosch3n <29953808+hosch3n@users.noreply.github.com>
2020-04-30 16:48:45 +0800

    Added t3version.py

---
+++ WebLogic相关隐患/t3version.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+from urllib.parse import urlparse
+import socket
+
+def parseURL(url):
+    _ = urlparse(url)
+
+    scheme = _.scheme
+    hostname = _.hostname
+    port = _.port
+
+    if (not port):
+        if (scheme == 'http'):
+            port = 80
+        elif (scheme == 'https'):
+            port = 443
+        else:
+            port = 7001
+
+    return hostname, port
+
+def t3conn(hostname, port):
+    sock = socket.create_connection((hostname, port), 3)
+    headers = b't3 10.3.6\nAS:255\nHL:19\n\n'
+    sock.sendall(headers)
+    data = sock.recv(1024)
+    sock.close()
+    return data
+
+hostname, port = parseURL(sys.argv[1])
+data = t3conn(hostname, port)
+if b'HELO' in data:
+    version = data[5:13]
+    print(str(version).strip("b'"))
