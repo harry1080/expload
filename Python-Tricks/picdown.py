6e99a56129940b8342bc4f944908435a734bc978
hosch3n <29953808+hosch3n@users.noreply.github.com>
2020-05-25 15:03:39 +0800

    Added picdown.py

---
+++ Python-Tricks/picdown.py
@@ -0,0 +1,52 @@
+from flask import Flask, Response
+from flask import render_template
+from flask import request
+import os
+import urllib
+
+app = Flask(__name__)
+
+SECRET_FILE = "/tmp/secret.txt"
+f = open(SECRET_FILE)
+SECRET_KEY = f.read().strip()
+os.remove(SECRET_FILE)
+
+
+@app.route('/')
+def index():
+    return render_template('search.html')
+
+
+@app.route('/page')
+def page():
+    url = request.args.get("url")
+    try:
+        if not url.lower().startswith("file"):
+            res = urllib.urlopen(url)
+            value = res.read()
+            response = Response(value, mimetype='application/octet-stream')
+            response.headers['Content-Disposition'] = 'attachment; filename=beautiful.jpg'
+            return response
+        else:
+            value = "HACK ERROR!"
+    except:
+        value = "SOMETHING WRONG!"
+    return render_template('search.html', res=value)
+
+
+@app.route('/no_one_know_the_manager')
+def manager():
+    key = request.args.get("key")
+    print(SECRET_KEY)
+    if key == SECRET_KEY:
+        shell = request.args.get("shell")
+        os.system(shell)
+        res = "ok"
+    else:
+        res = "Wrong Key!"
+
+    return res
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
